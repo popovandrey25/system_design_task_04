902186cdf0a5e0a87528ea9a109f7317f7ea932e
andrey <palanelarty@gmail.com>
2025-04-20 11:49:41 +0300

    Изменил методы в репозитории для работы с маршрутами

--- route_service/app/repositories/route_repository.py
+++ route_service/app/repositories/route_repository.py
@@ -1,24 +1,41 @@
 from typing import List, Optional
-from app.db import fake_routes_db, get_next_route_id
+from bson import ObjectId
+
 from app.models import Route
+from app.db import db
 
 
 class RouteRepository:
-    """
-    Репозиторий для CRUD операций по маршрутам.
-    """
+    collection = db.routes
 
     @staticmethod
-    def create_route(start: str, end: str, waypoints: list[str]) -> Route:
-        new_id = get_next_route_id()
-        new_route = Route(route_id=new_id, start_point=start, end_point=end, waypoints=waypoints)
-        fake_routes_db[new_id] = new_route
-        return new_route
+    async def create_route(start: str, end: str, waypoints: list[str]) -> Route:
+        doc = {"start_point": start, "end_point": end, "waypoints": waypoints}
+        result = await RouteRepository.collection.insert_one(doc)
+        return Route(route_id=str(result.inserted_id), start_point=start, end_point=end, waypoints=waypoints)
 
     @staticmethod
-    def get_route_by_id(route_id: int) -> Optional[Route]:
-        return fake_routes_db.get(route_id)
+    async def get_route_by_id(route_id: str) -> Optional[Route]:
+        doc = await RouteRepository.collection.find_one({"_id": ObjectId(route_id)})
+        if not doc:
+            return None
+        return Route(
+            route_id=str(doc["_id"]),
+            start_point=doc["start_point"],
+            end_point=doc["end_point"],
+            waypoints=doc.get("waypoints", [])
+        )
 
     @staticmethod
-    def get_all_routes() -> List[Route]:
-        return list(fake_routes_db.values())
+    async def get_all_routes() -> List[Route]:
+        routes: List[Route] = []
+        async for doc in RouteRepository.collection.find():
+            routes.append(
+                Route(
+                    route_id=str(doc["_id"]),
+                    start_point=doc["start_point"],
+                    end_point=doc["end_point"],
+                    waypoints=doc.get("waypoints", [])
+                )
+            )
+        return routes
