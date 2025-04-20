800e4f41a4ad994e6ae7a2d32be6876364b79af3
andrey <palanelarty@gmail.com>
2025-04-20 11:50:07 +0300

    Изменил роутер

--- route_service/app/routers/route_router.py
+++ route_service/app/routers/route_router.py
@@ -1,4 +1,5 @@
 from typing import List
+
 from fastapi import APIRouter, Depends, HTTPException
 
 from app.schemas import RouteCreateRequest, RouteResponse
@@ -8,35 +9,36 @@ from app.auth import verify_token
 
 router = APIRouter(prefix="/routes", tags=["routes"])
 
-route_service = RouteService(repo=RouteRepository())
-
+service = RouteService(repo=RouteRepository())
 
 @router.post("", response_model=RouteResponse, dependencies=[Depends(verify_token)])
-def create_route(data: RouteCreateRequest):
+async def create_route(data: RouteCreateRequest):
     """
     Создать новый маршрут
     """
-    route = route_service.create_new_route(
-        start=data.start_point, end=data.end_point, waypoints=data.waypoints or []
+    route = await service.create_new_route(
+        start=data.start_point,
+        end=data.end_point,
+        waypoints=data.waypoints or []
     )
     return route.dict()
 
 
 @router.get("/{route_id}", response_model=RouteResponse, dependencies=[Depends(verify_token)])
-def get_route_by_id(route_id: int):
+async def get_route_by_id(route_id: str):
     """
     Получить маршрут по его ID
     """
-    route = route_service.get_route(route_id)
+    route = await service.get_route(route_id)
     if not route:
         raise HTTPException(status_code=404, detail="Route not found")
     return route.dict()
 
 
 @router.get("", response_model=List[RouteResponse], dependencies=[Depends(verify_token)])
-def list_all_routes():
+async def list_all_routes():
     """
     Получить список всех маршрутов
     """
-    routes = route_service.list_routes()
+    routes = await service.list_routes()
     return [r.dict() for r in routes]
